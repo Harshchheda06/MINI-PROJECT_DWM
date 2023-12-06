b167ed1c54f73c8c03d5a17ac21feb4ac39d8c30
harsh <chhedaharsh06@gmail.com>
2023-12-06 18:00:26 +0530

    if-else

--- data_warehouse-app.py
+++ data_warehouse-app.py
@@ -22,10 +22,27 @@ def calculate_mean(data, target_column):
 
 
 # Function to perform clustering using K-Means
-def perform_clustering(data):
-    kmeans = KMeans(n_clusters=2)
-    labels = kmeans.fit_predict(data)
-    return labels
+def perform_clustering(data,target_column, num_clusters):
+
+    return data
+
+# def perform_clustering(data,num_clusters):
+    # Select only relevant columns for clustering
+    
+    # target_column = st.selectbox("Select Target Column for Clustering", df.columns)
+    # Select only relevant columns for clustering
+    # clustering_data = data[target_column]
+    # Reshape the data to 2D array if it's 1D
+    # if len(clustering_data.shape) == 1:
+        # clustering_data = clustering_data.values.reshape(-1, 1)
+
+        # kmeans = KMeans(n_clusters=num_clusters)
+        # labels = kmeans.fit_predict(clustering_data)
+
+        # # Optionally, you can add the cluster labels as a new column in the dataframe
+        # data['Cluster'] = labels
+
+    # return data
 
 # Function to perform classification using Random Forest
 def perform_classification(data, target_column):
@@ -52,11 +69,43 @@ def perform_regression(data, target_column, independent_columns):
 
     reg = RandomForestRegressor()
     reg.fit(X, y)
+    # Visualization: Scatter Plot
+    st.subheader("Visualization: Scatter Plot")
+    plt.figure(figsize=(8, 6))
+    plt.scatter(data[independent_columns].iloc[:, 0], y, color='blue', label='Actual')
+    plt.scatter(data[independent_columns].iloc[:, 0], reg.predict(X), color='red', label='Predicted')
+    plt.title(f'Scatter Plot for {target_column} vs. {independent_columns[0]}')
+    plt.xlabel(independent_columns[0])
+    plt.ylabel(target_column)
+    plt.legend()
+    st.pyplot(plt)
+
+    # Visualization: Box Plot
+    st.subheader("Visualization: Box Plot")
+    plt.figure(figsize=(8, 6))
+    sns.boxplot(x=data[target_column], y=data[independent_columns[0]])
+    plt.title(f'Box Plot for {target_column} vs. {independent_columns[0]}')
+    plt.xlabel(target_column)
+    plt.ylabel(independent_columns[0])
+    st.pyplot(plt)
+
+    # Visualization: Feature Importance
+    st.subheader("Visualization: Feature Importance")
+    feature_importance = reg.feature_importances_
+    importance_df = pd.DataFrame({'Feature': independent_columns, 'Importance': feature_importance})
+    importance_df = importance_df.sort_values(by='Importance', ascending=False)
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='Importance', y='Feature', data=importance_df)
+    plt.title('Feature Importance for Regression')
+    plt.xlabel('Importance')
+    plt.ylabel('Feature')
+    st.pyplot(plt)
 
     # Return predicted values
     return reg.predict(X)
 
-# Function to perform outlier detection using One-Class SVM
+# Function to perform outlier detection using OneClass SVM
 def detect_outliers(data):
     ocsvm = OneClassSVM()
     labels = ocsvm.fit_predict(data)
@@ -68,7 +117,7 @@ def main():
 
     # Upload dataset
     uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-    
+
     if uploaded_file is not None:
         # Read the dataset
         df = pd.read_csv(uploaded_file)
@@ -84,6 +133,11 @@ def main():
         if operation in ["Classification", "Regression", "Mean"]:
             target_column = st.selectbox("Select Target Column", df.columns)
 
+        if operation in["Clustering"]:
+            num_clusters = st.slider("Select Number of Clusters", min_value=2, max_value=10, value=3)
+            target_column1 = st.selectbox("Select Target Column", df.columns, key="target_column1")
+            target_column2 = st.selectbox("Select Target Column", df.columns, key="target_column2")
+
         # User input for independent columns (for regression)
         independent_columns = []
         if operation == "Regression":
@@ -94,7 +148,16 @@ def main():
             if operation == "Mean":
                 result = calculate_mean(df, target_column)
             elif operation == "Clustering":
-                result = perform_clustering(df)
+               
+                result = perform_clustering(df,num_clusters)
+
+                # Visualization: Scatter Plot with Clusters
+                st.subheader("Visualization: Scatter Plot with Clusters")
+                plt.figure(figsize=(8, 6))
+                sns.scatterplot(data=result, x=target_column1, y=target_column2, hue='Cluster', palette='viridis', legend='full')
+                plt.title(f'Scatter Plot with {num_clusters} Clusters')
+                st.pyplot(plt)
+
             elif operation == "Classification":
                 result = perform_classification(df, target_column)
             elif operation == "Association Rule Mining":
